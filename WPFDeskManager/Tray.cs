4f1dd6a34d39d1b854a70abb2071b1a3a28e5b5b
shenzhiqiang <593446878@qq.com>
2025-04-28 00:36:44 +0800

    好吧，我现在认命了。

--- WPFDeskManager/Tray.cs
+++ WPFDeskManager/Tray.cs
@@ -1,21 +1,23 @@
-﻿using System.Windows;
-using System.Windows.Controls.Primitives;
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Resources;
 
 namespace WPFDeskManager
 {
     internal class Tray
     {
-        /*public TaskbarIcon TrayIcon;*/
+        public TaskbarIcon TrayIcon;
 
         public Tray()
         {
             StreamResourceInfo streamInfo = Application.GetResourceStream(new Uri("pack://application:,,,/Assets/logo.ico"));
-            /*this.TrayIcon = new TaskbarIcon
+            this.TrayIcon = new TaskbarIcon
             {
                 Icon = new System.Drawing.Icon(streamInfo.Stream),
                 ToolTipText = "桌面整理工具"
-            };*/
+            };
         }
     }
 }
